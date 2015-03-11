387bfa0c482e96682c8bf8c5b602ddd364627aa0
Jawad Tahir <ranajawadtahir@gmail.com>
2015-03-11 19:56:38 +0500

    tcb

--- src/Threading.cpp
+++ src/Threading.cpp
@@ -12,12 +12,26 @@
 #include <list>
 using namespace std;
 
-list<ucontext_t*> ready_q, thread_created;
-ucontext_t main_context;
+
+struct tcb_t{
+	int thread_id;
+	ucontext_t* context;
+	int status;
+};
+
+
+list<tcb_t> ready_q, thread_created;
+tcb_t main_context_tcb;
+
+
 
 
 void mythread_init()
 {
+	ucontext_t main_context;
+	main_context_tcb.context = &main_context;
+	main_context_tcb.thread_id = -1;
+	main_context_tcb.status = 1;
 	getcontext(&main_context);
 	//cout<<"R"<<endl;
 	main_context.uc_stack.ss_sp = new char[SIGSTKSZ];
@@ -29,18 +43,22 @@ int mythread_fork()
 	int* resumed = &a;
 	*resumed = 0;
 	ucontext_t new_context;
-	getcontext(&new_context);
-	//getcontext(&main_context);
-	new_context.uc_link = &main_context;
+	tcb_t new_context_tcb;
+	new_context_tcb.context = &new_context;
+	new_context_tcb.thread_id = thread_created.size()+1;
+	new_context_tcb.status = 1;
+	main_context_tcb.status = 0;
+	getcontext(new_context_tcb.context);
+	getcontext(main_context_tcb.context);
+	new_context.uc_link = main_context_tcb.context;
 	new_context.uc_stack.ss_sp = new char[SIGSTKSZ];
 	new_context.uc_stack.ss_size = SIGSTKSZ;
 	//swapcontext(&new_context, &main_context);
-	thread_created.push_back(&new_context);
-	ready_q.push_back(&main_context);
-	getcontext(&new_context);
-	new_context.uc_link = &main_context;
-	getcontext(&main_context);
-	cout<<"Hello"<<endl;
+	thread_created.push_back(new_context_tcb);
+	ready_q.push_back(main_context_tcb);
+	getcontext(new_context_tcb.context);
+	new_context.uc_link = main_context_tcb.context;
+	getcontext(main_context_tcb.context);
 	if (*resumed == 0)
 	{
 		*resumed = 1;
@@ -49,7 +67,7 @@ int mythread_fork()
 	else
 	{
 		*resumed = 0;
-		ret_val = thread_created.size();
+		ret_val = new_context_tcb.thread_id;
 	}
 
 
@@ -61,16 +79,18 @@ int mythread_fork()
 void mythread_exit(){}
 void mythread_yield()
 {
+	tcb_t temp_tcb;
 	ucontext_t new_context;
 	ucontext_t* temp_context;
+
 	getcontext(&new_context);
 
 	ready_q.push_back(&new_context);
 	temp_context = ready_q.front();
 	ready_q.pop_front();
-	setcontext(temp_context);
+	//setcontext(temp_context);
 
-	//swapcontext(&new_context, temp_context);
+	swapcontext(&new_context, temp_context);
 }
 int mythread_join(int threadId){}
 
@@ -92,7 +112,7 @@ int main() {
 	mythread_init();
 	std::cout << "A" << std::endl;
 	int thread1 = mythread_fork();
-	setcontext(&main_context);
+	//setcontext(&main_context);
 	if (thread1==0)
 		MyExampleThread1();
 	std::cout << "C" << std::endl;
